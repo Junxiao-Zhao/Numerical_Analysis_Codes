dd83ebd9d0892e0d3a047e594a4e9edd43ae1a7b
Junxiao <zhao_junxiao@hotmail.com>
2023-05-10 12:26:36 -0400

    HW8 Update

--- HW8/2D_Lagrange.py
+++ HW8/2D_Lagrange.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 
 def l0(x):
@@ -24,13 +25,27 @@ plt.plot(xs, l2(xs), label="$L_2$")
 plt.legend()
 plt.savefig("Lagrange basis functions.png")
 
-plt.figure()
-plt.title("2D Lagrange basis functions")
+X, Y = np.meshgrid(xs, xs)
+
+fig, axes = plt.subplots(3,
+                         3,
+                         figsize=(10, 10),
+                         subplot_kw={'projection': '3d'})
+# fig = plt.figure(figsize=(18, 18))
+# ax = plt.axes(projection='3d')
+fig.suptitle("2D Lagrange basis functions")
 ls = [l0, l1, l2]
 for m, lm in enumerate(ls):
     for n, ln in enumerate(ls):
-        plt.plot(xs, lm(xs) * ln(xs), label=r"$L_{%s%s}$" % (m, n))
-plt.legend()
+        ax = axes[m, n]
+        ax.plot_surface(X, Y, lm(X) * ln(Y), cmap='viridis')
+        ax.set_title(r"$L_{%s%s}$" % (m, n))
+        ax.set_xlabel("x")
+        ax.set_ylabel("y")
+        ax.set_zlabel(r"$L_{%s%s}$" % (m, n))
+
+        # plt.plot(xs, lm(xs) * ln(xs), label=r"$L_{%s%s}$" % (m, n))
+# plt.legend()
 plt.savefig("2D Lagrange basis functions.png")
 
 plt.show()
